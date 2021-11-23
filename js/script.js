69b01fc3d0e1728b1df9d2fa1eb0772bb431a1a8
MatteoRuggieri2 <matteoruggieri2@gmail.com>
2021-11-23 20:00:24 +0100

    Aggiunti i numeri dentro gli square

--- js/script.js
+++ js/script.js
@@ -7,12 +7,89 @@
 // Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro.
 
 
-// devo prendere la scelta di difficoltà dell'utente
-const userChoice = document.querySelector('.choice-list').value;
-console.log(userChoice);
+
+let userChoice = '';
+document.getElementById("play-button").addEventListener("click", function() {
+    // prendo la scelta di difficoltà dell'utente
+    userChoice = document.querySelector('.choice-list').value;
+    const grid = document.querySelector('.grid');
+    grid.innerHTML = '';
+
+
+    let squareSize = '';
+    let squareQuantity = 0;
+    if (userChoice == 'easy') {
+        squareSize = 'square-10';
+        squareQuantity = 100;
+
+    } else if (userChoice == 'hard') {
+        squareSize = 'square-9';
+        squareQuantity = 81;
+
+    } else if (userChoice == 'crazy') {
+        squareSize = 'square-7';
+        squareQuantity = 49;
+    }
+
+    for (let i = 0; i < squareQuantity; i++) {
+        const numeri = generateRandomNumber(squareQuantity);
+
+        const newBox = `
+        <div class="square ${squareSize}">
+            <span>${numeri[i]}</span>
+        </div>
+        `
+        grid.innerHTML += newBox
+        console.log(squareSize);
+    }
+
+
+
+});
+
+
+
+
 
 // se sceglie facile metto quadrati s-10 e numeri da 1 e 100
 
 // se sceglie medio metto quadrati s-9 e numeri da 1 e 81
 
 // se sceglie difficile metto quadrati s-7 e numeri da 1 e 49
+
+
+
+
+// FUNCTION
+// Creo una funzione che mi da sotto forma di array numeri 
+// tra 1 e 100, tra 1 e 81, e tra 1 e 49 senza ripeterli
+
+
+
+// Questa funzione serve a generare una quantità di numeri
+// casuali scelti da noi tramite l'argomento, che non si ripetono mai.
+// return: fornisce un array contenente i numeri univoci
+function generateRandomNumber (quantity) {
+    const numbersArray = [];
+    while(numbersArray.length < quantity) {
+
+        const randomNumber = getRndInteger(1, quantity);
+        if (!numbersArray.includes(randomNumber)) {
+            numbersArray.push(randomNumber)
+        }
+
+    }
+    return numbersArray
+
+}
+
+// Questa funzione serve a generare numeri casuali da un min a un max 
+// (tutti e due compresi) che diamo noi come argomento.
+// return: ci fornisce un numero
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+
+
+
+
